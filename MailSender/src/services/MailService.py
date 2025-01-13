15b2ef3d13ac7638d3acdaa4706883b82ef1a016
enmarcm <theenmanuel123@gmail.com>
2025-01-13 18:37:48 -0400

    refactor(MailServer-MailService): update logging logic

--- MailSender/src/services/MailService.py
+++ MailSender/src/services/MailService.py
@@ -5,4 +5,4 @@ class MailService(mail_pb2_grpc.MailServiceServicer):
     def SendMail(self, request, context):
         print(f"Received mail request: {request.email}, {request.subject}, {request.body}")
         # Aquí puedes agregar la lógica para enviar el correo
-        return mail_pb2.MailResponse(status="Mail sent successfully")
+        return mail_pb2.MailResponse(status=f"Mail sent successfully to {request.email}" )
