2a5c8db0b8b173897f153987a063a5c1a50fd44d
enmarcm <theenmanuel123@gmail.com>
2025-01-14 17:53:05 -0400

    refactor(MailSender-MailService): adjust from email to to for SendMail request

--- MailSender/src/protos/mail_pb2.py
+++ MailSender/src/protos/mail_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nmail.proto\x12\x04mail\";\n\x0bMailRequest\x12\r\n\x05\x65mail\x18\x01 \x01(\t\x12\x0f\n\x07subject\x18\x02 \x01(\t\x12\x0c\n\x04\x62ody\x18\x03 \x01(\t\"\x1e\n\x0cMailResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\"-\n\nSMSRequest\x12\x0e\n\x06number\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\"\x1d\n\x0bSMSResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\"\x19\n\nLogRequest\x12\x0b\n\x03log\x18\x01 \x01(\t\"\x1d\n\x0bLogResponse\x12\x0e\n\x06status\x18\x01 \x01(\t2B\n\x0bMailService\x12\x33\n\x08SendMail\x12\x11.mail.MailRequest\x1a\x12.mail.MailResponse\"\x00\x32>\n\nSMSService\x12\x30\n\x07SendSMS\x12\x10.mail.SMSRequest\x1a\x11.mail.SMSResponse\"\x00\x32?\n\nLogService\x12\x31\n\x08SendLogs\x12\x10.mail.LogRequest\x1a\x11.mail.LogResponse\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nmail.proto\x12\x04mail\"8\n\x0bMailRequest\x12\n\n\x02to\x18\x01 \x01(\t\x12\x0f\n\x07subject\x18\x02 \x01(\t\x12\x0c\n\x04\x62ody\x18\x03 \x01(\t\"\x1e\n\x0cMailResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\"-\n\nSMSRequest\x12\x0e\n\x06number\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\"\x1d\n\x0bSMSResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\"\x19\n\nLogRequest\x12\x0b\n\x03log\x18\x01 \x01(\t\"\x1d\n\x0bLogResponse\x12\x0e\n\x06status\x18\x01 \x01(\t2B\n\x0bMailService\x12\x33\n\x08SendMail\x12\x11.mail.MailRequest\x1a\x12.mail.MailResponse\"\x00\x32>\n\nSMSService\x12\x30\n\x07SendSMS\x12\x10.mail.SMSRequest\x1a\x11.mail.SMSResponse\"\x00\x32?\n\nLogService\x12\x31\n\x08SendLogs\x12\x10.mail.LogRequest\x1a\x11.mail.LogResponse\"\x00\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -32,21 +32,21 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'mail_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_MAILREQUEST']._serialized_start=20
-  _globals['_MAILREQUEST']._serialized_end=79
-  _globals['_MAILRESPONSE']._serialized_start=81
-  _globals['_MAILRESPONSE']._serialized_end=111
-  _globals['_SMSREQUEST']._serialized_start=113
-  _globals['_SMSREQUEST']._serialized_end=158
-  _globals['_SMSRESPONSE']._serialized_start=160
-  _globals['_SMSRESPONSE']._serialized_end=189
-  _globals['_LOGREQUEST']._serialized_start=191
-  _globals['_LOGREQUEST']._serialized_end=216
-  _globals['_LOGRESPONSE']._serialized_start=218
-  _globals['_LOGRESPONSE']._serialized_end=247
-  _globals['_MAILSERVICE']._serialized_start=249
-  _globals['_MAILSERVICE']._serialized_end=315
-  _globals['_SMSSERVICE']._serialized_start=317
-  _globals['_SMSSERVICE']._serialized_end=379
-  _globals['_LOGSERVICE']._serialized_start=381
-  _globals['_LOGSERVICE']._serialized_end=444
+  _globals['_MAILREQUEST']._serialized_end=76
+  _globals['_MAILRESPONSE']._serialized_start=78
+  _globals['_MAILRESPONSE']._serialized_end=108
+  _globals['_SMSREQUEST']._serialized_start=110
+  _globals['_SMSREQUEST']._serialized_end=155
+  _globals['_SMSRESPONSE']._serialized_start=157
+  _globals['_SMSRESPONSE']._serialized_end=186
+  _globals['_LOGREQUEST']._serialized_start=188
+  _globals['_LOGREQUEST']._serialized_end=213
+  _globals['_LOGRESPONSE']._serialized_start=215
+  _globals['_LOGRESPONSE']._serialized_end=244
+  _globals['_MAILSERVICE']._serialized_start=246
+  _globals['_MAILSERVICE']._serialized_end=312
+  _globals['_SMSSERVICE']._serialized_start=314
+  _globals['_SMSSERVICE']._serialized_end=376
+  _globals['_LOGSERVICE']._serialized_start=378
+  _globals['_LOGSERVICE']._serialized_end=441
 # @@protoc_insertion_point(module_scope)
--- MailSender/src/services/MailService.py
+++ MailSender/src/services/MailService.py
@@ -3,8 +3,9 @@ from protos import mail_pb2
 
 class MailService(mail_pb2_grpc.MailServiceServicer):
     def SendMail(self, request, context):
-        print(f"Received mail request: {request.email}, {request.subject}, {request.body}")
+        print(request)
+        print(f"Received mail request: {request.to}, {request.subject}, {request.body}")
         # Aquí puedes agregar la lógica para enviar el correo
-        return mail_pb2.MailResponse(status=f"Mail sent successfully to {request.email}" )
+        return mail_pb2.MailResponse(status=f"Mail sent successfully to {request.to}" )
     
     
