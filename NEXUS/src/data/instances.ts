a9627df7af1e2938d946bac368ec25ce6714eed4
enmarcm <theenmanuel123@gmail.com>
2025-01-14 17:48:32 -0400

    refactor(instances): change instance from _NexusProto to Nexus and add test data and servers variables

--- NEXUS/src/data/instances.ts
+++ NEXUS/src/data/instances.ts
@@ -1,12 +1,31 @@
-// In this file is where we define the instances of the classes that we want to use in our application.
-
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 import LogHistory from "../BO/LogHistory/LogHistory";
-import Nexus from "../BO/Nexus/_NexusProto";
+import Nexus from "../BO/Nexus/Nexus";
 
-export const iNexus = new Nexus();
+const data = [
+  {
+    type: "EMAIL" as const,
+    content: "theenmanuel123@gmail.com",
+    status: "ACTIVE" as const,
+    rest: 1000,
+    credentials: {},
+  },
+];
+
+const servers = [
+  {
+    name: "MailServer1",
+    host: "0.0.0.0",
+    port: 50051,
+    status: "ONLINE" as const,
+    use: "FREE" as const,
+    typeInfo: "EMAIL" as const,
+  },
+];
+
+export const iNexus = new Nexus({ data, servers });
 
 const uri = process.env.URI_MONGO || "";
 const collectionName = process.env.COLLECTION_LOGS || "";
