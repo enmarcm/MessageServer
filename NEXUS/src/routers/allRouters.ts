9d6dc9fe2c9a1bd22db06fccb14d2826f88809c4
enmarcm <theenmanuel123@gmail.com>
2025-01-14 17:47:19 -0400

    refactor(allRouters): change sendMail request from email to to as used in sendMail

--- NEXUS/src/routers/allRouters.ts
+++ NEXUS/src/routers/allRouters.ts
@@ -5,15 +5,14 @@ import { iNexus, logger } from "../data/instances";
 export const MainRouter = Router();
 
 MainRouter.get("/", async (data: ReqRes) => {
-  const result = await iNexus.sendMail({
-    email: "hermandad@yahoo.com",
+   await iNexus.sendMail({
+    to: "hermandad@yahoo.com",
     subject: "Prueba",
     body: "Este es un contenido del correo, nos avisa que tal",
   });
 
-  logger.log(result)
 
-  data.res.send(`<h1>Este fue el resultado${JSON.stringify(result)}</h1>`);
+  data.res.send(`<h1>Creo que si se envio</h1>`);
 });
 
 MainRouter.get("/showLogs", async(data: ReqRes)=>{
