922c08a2f86c41c230a97bd26f0cb954a3a8f833
enmarcm <theenmanuel123@gmail.com>
2025-02-27 16:23:55 -0400

    feat(Nexus-ApiKey): add default uses configuration and generateApiKey method in ApiKeyHandler

--- NEXUS/src/utils/ApiKeyHandler.ts
+++ NEXUS/src/utils/ApiKeyHandler.ts
@@ -1,5 +1,7 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
+import CryptManager from "./CryptManager";
+import config from "../config.json";
 
 interface ApiKeyData {
   apiKey: string;
@@ -18,8 +20,8 @@ class ApiKeyHandler {
    * Creates an instance of ApiKeyHandler.
    */
   constructor() {
-    this.dataPath = path.join(__dirname, '../data/jsons/apiKey.json');
-    const data = fs.readFileSync(this.dataPath, 'utf-8');
+    this.dataPath = path.join(__dirname, "../data/jsons/apiKey.json");
+    const data = fs.readFileSync(this.dataPath, "utf-8");
     this.apiKeys = JSON.parse(data) as ApiKeyData[];
   }
 
@@ -29,7 +31,7 @@ class ApiKeyHandler {
    * @returns {boolean} - True if the API key is valid, false otherwise.
    */
   public validateApiKey(key: string): boolean {
-    const apiKeyData = this.apiKeys.find(apiKey => apiKey.apiKey === key);
+    const apiKeyData = this.apiKeys.find((apiKey) => apiKey.apiKey === key);
     if (!apiKeyData || apiKeyData.remainingUses <= 0) {
       return false;
     }
@@ -41,7 +43,7 @@ class ApiKeyHandler {
    * @param {string} key - The API key to update.
    */
   public updateApiKeyUsage(key: string): void {
-    const apiKeyData = this.apiKeys.find(apiKey => apiKey.apiKey === key);
+    const apiKeyData = this.apiKeys.find((apiKey) => apiKey.apiKey === key);
     if (apiKeyData && apiKeyData.remainingUses > 0) {
       apiKeyData.remainingUses -= 1;
       this.saveApiKeys();
@@ -53,7 +55,28 @@ class ApiKeyHandler {
    * @private
    */
   private saveApiKeys(): void {
-    fs.writeFileSync(this.dataPath, JSON.stringify(this.apiKeys, null, 2), 'utf-8');
+    fs.writeFileSync(
+      this.dataPath,
+      JSON.stringify(this.apiKeys, null, 2),
+      "utf-8"
+    );
+  }
+
+  /**
+   * Generates a new API key and adds it to the list of valid API keys.
+   * @returns {string} - The newly generated API key.
+   */
+  public generateApiKey(): string {
+    const newApiKey: ApiKeyData = {
+      apiKey: CryptManager.generateRandom({ size: 6 }),
+      remainingUses: config.security.API_KEY.default_uses,
+      user: "",
+    };
+
+    this.apiKeys.push(newApiKey);
+    this.saveApiKeys();
+
+    return newApiKey.apiKey;
   }
 }
 
