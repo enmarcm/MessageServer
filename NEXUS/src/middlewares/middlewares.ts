b801acc80d102f3b2b87426eafe1b993d238cf62
enmarcm <theenmanuel123@gmail.com>
2025-02-27 13:17:11 -0400

    feat(Nexus-middlewares): add import and export for handling JWTokens and sessions

--- NEXUS/src/middlewares/middlewares.ts
+++ NEXUS/src/middlewares/middlewares.ts
@@ -3,10 +3,11 @@ import express from "express";
 import midNotJson from "./midNotJson";
 import midNotFound from "./midNotFound";
 import midValidJson from "./midValidJson";
+import midToken from "./midToken";
 import { midApiKey } from "./midApiKey";
 
 export const midJson = () => express.json();
 
 export const midCors = () => cors({ credentials: true, origin: "*" });
 
-export { midNotJson, midNotFound, midValidJson, midApiKey };
+export { midNotJson, midNotFound, midValidJson, midApiKey, midToken };
