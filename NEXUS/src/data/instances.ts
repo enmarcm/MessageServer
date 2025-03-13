c7ee398e3c21c87c999908767e5e5a75e85b887d
enmarcm <theenmanuel123@gmail.com>
2025-03-13 05:24:36 -0400

    feat(instances): create instance of security module

--- NEXUS/src/data/instances.ts
+++ NEXUS/src/data/instances.ts
@@ -6,11 +6,12 @@ export const ITSGooseHandler = new TSGooseHandler(CONNECTION_STRING);
 
 import Nexus from "../BO/Nexus/Nexus";
 import JWTManager from "../utils/JWTManager";
-
 import LogHistory from "../BO/LogHistory/LogHistory";
 import PgHandler from "../utils/PgHandler";
 import config from "../config.json";
 import querys from "../data/jsons/querys.json";
+import Security from "../utils/Security";
+import PermissionController from "../BO/controllers/PermissionController";
 
 const data = [
   {
@@ -31,6 +32,15 @@ const servers = [
     use: "FREE" as const,
     typeInfo: "EMAIL" as const,
   },
+  {
+    name: "SMSServer1",
+    host: "0.0.0.0",
+    port: 50052,
+    status: "ONLINE" as const,
+    use: "FREE" as const,
+    typeInfo: "SMS" as const,
+  },
+
 ];
 
 export const iNexus = new Nexus({ data, servers });
@@ -40,3 +50,5 @@ export const logger = new LogHistory();
 export const IJWTManager = new JWTManager(JWT_CONFIG);
 
 export const iPgHandler = new PgHandler({ config: config.database, querys });
+
+export const iSecurity = new Security({ controller: PermissionController, config: config.security.moduleSecurity });
