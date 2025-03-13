aca92ceba5beff2ebdb96448ae48e29cf598931a
enmarcm <theenmanuel123@gmail.com>
2025-03-13 05:23:35 -0400

    chore(build): Remove js files

--- NEXUS/build/src/middlewares/midNotFound.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const midNotFound = (_req, res, next) => {
-    res.status(404).json({ error: "Not Found" });
-    next();
-};
-exports.default = midNotFound;
